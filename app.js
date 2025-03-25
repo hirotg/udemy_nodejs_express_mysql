d2c504c8efb50de8dcbd3e7ad0ef357bbbbbccd0
hirotg <hirokazutg@gmail.com>
2025-03-25 11:39:44 +0000

    23 終了

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 require('dotenv').config();
- 
-PORT = process.env.PORT;
-console.log(`Application listening at ${PORT}`);
+
+const PORT = process.env.PORT;
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res) =>{
+    res.end("Hello World");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Application listening at ${PORT}`);
+});
